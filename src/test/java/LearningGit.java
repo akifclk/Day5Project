d7434161536d183ef7a0943c299018df59ae1810
akifclk <akifclk91@gmail.com>
2020-10-15 22:03:08 +0200

    initial commit

--- src/test/java/LearningGit.java
+++ src/test/java/LearningGit.java
@@ -5,5 +5,9 @@ public class LearningGit {
         //Enable version control integration from VCS
         //add .gitignore -> .idea , *.iml, target etc. from right click git -> add to .gitignore we can rollback too
         //commit from green icon
+        // share project on github from VCS -> import into version control
+
+        //create new branch bottom right button
+
     }
 }
