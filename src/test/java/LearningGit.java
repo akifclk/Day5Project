81b4bf2f2dd35550ef1af5a4e531ac4702443baf
akifclk <akifclk91@gmail.com>
2020-10-15 21:52:40 +0200

    initial commit

---
+++ src/test/java/LearningGit.java
@@ -0,0 +1,9 @@
+public class LearningGit {
+
+    public static void main(String[] args) {
+
+        //Enable version control integration from VCS
+        //add .gitignore -> .idea , *.iml, target etc. from right click git -> add to .gitignore we can rollback too
+        //commit from green icon
+    }
+}
